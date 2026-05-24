e4b249493f6e696737248b7902afff598b7f4598
elmir <elmireas@code.edu.az>
2026-05-24 17:36:35 +0400

    Revert "feat(home): add premium hospital showcase section"
    This reverts commit b1d5e9a0607456d854701612562ecc9e7b8ee028.

--- client/src/pages/public/HomePage.jsx
+++ client/src/pages/public/HomePage.jsx
@@ -1,17 +1,17 @@
 import HeroSection      from '../../components/HeroSection';
 import PatientStories   from '../../components/sections/PatientStories';
 import ClinicalPrograms from '../../components/sections/ClinicalPrograms';
-import HospitalShowcase from '../../components/sections/HospitalShowcase';
+import AwardBanner      from '../../components/sections/AwardBanner';
 import DoctorsSection   from '../../components/sections/DoctorsSection';
 
 export default function HomePage() {
   return (
     <main>
       <HeroSection />
-      <HospitalShowcase />
       <ClinicalPrograms />
-      <PatientStories />
+      <AwardBanner />
       <DoctorsSection />
+      <PatientStories />
     </main>
   );
 }
