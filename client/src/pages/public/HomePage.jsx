b59696f482044f9f3922ebb39090d8a252b978a8
elmir <elmireas@code.edu.az>
2026-05-24 17:51:10 +0400

    fix: restore HospitalShowcase and HomePage

--- client/src/pages/public/HomePage.jsx
+++ client/src/pages/public/HomePage.jsx
@@ -1,17 +1,17 @@
 import HeroSection      from '../../components/HeroSection';
 import PatientStories   from '../../components/sections/PatientStories';
 import ClinicalPrograms from '../../components/sections/ClinicalPrograms';
-import AwardBanner      from '../../components/sections/AwardBanner';
+import HospitalShowcase from '../../components/sections/HospitalShowcase';
 import DoctorsSection   from '../../components/sections/DoctorsSection';
 
 export default function HomePage() {
   return (
     <main>
       <HeroSection />
+      <HospitalShowcase />
       <ClinicalPrograms />
-      <AwardBanner />
-      <DoctorsSection />
       <PatientStories />
+      <DoctorsSection />
     </main>
   );
 }
